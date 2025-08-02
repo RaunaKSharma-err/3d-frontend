c4e9bf626435250928b64a1209c636f1c2742a30
RaunaKSharma-err <progritik@gmail.com>
2025-08-02 22:03:34 +0545

    init

--- store/models.py
+++ store/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 
 
+class Collection(models.Model):
+    title = models.CharField(max_length=200)
+
+
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
 
 
 class Customer(models.Model):
@@ -37,3 +42,27 @@ class Order(models.Model):
     ]
     placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES)
+    costumer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+
+
+class OrderItem(models.Model):
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+
+
+class Address(models.Model):
+    street = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+
+class Cart(models.Model):
+    createdAt = models.DateTimeField(auto_now_add=True)
+
+
+class CartItem(models.Model):
+    quantity = models.PositiveSmallIntegerField()
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
