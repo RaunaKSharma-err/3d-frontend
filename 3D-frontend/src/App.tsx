4ceb507fc838e08744fe5e2f70c2a99d64ec82c7
RaunaKSharma-err <progritik@gmail.com>
2025-08-13 21:04:20 +0545

    init

---
+++ 3D-frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="h-[100vh] flex justify-center items-center">
+        <h1>hello world</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
