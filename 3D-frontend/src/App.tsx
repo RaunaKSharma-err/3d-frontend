d9670482cd1448e48a8f9a63b51928feef238232
RaunaKSharma-err <progritik@gmail.com>
2025-08-16 16:30:25 +0545

    init

--- 3D-frontend/src/App.tsx
+++ 3D-frontend/src/App.tsx
@@ -1,33 +1,30 @@
 import Robot_model from "./components/Robot_model";
-import { Button } from "./components/ui/button";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import { useEffect, useState } from "react";
+
 function App() {
+  const [paddingClass, setPaddingClass] = useState("p-10");
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setPaddingClass("p-3");
+    }, 300);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
     <>
-      <div className="h-[100vh] relative flex justify-center items-center bg-[#ebebeb]">
-        <div className="absolute flex top-[30%] ml-24 text-9xl text-gray-300 z-0 tracking-[80px] text-shadow-2xs font-bold .lavishly-yours-regular">
-          FRON TEND
-        </div>
-        <div className="absolute top-0 h-[80px] w-[100%] text-5xl flex justify-between px-5 items-center">
-          <div className="flex justify-between items-center h-[80px]">
-            <img src="A.png" alt="Logo" className="h-30 mt-8" />
-            <div className="text-xl gap-10 ml-10 flex font-bold">
-              <a className="z-50" href="">
-                Home
-              </a>
-              <a className="z-50" href="">
-                Products
-              </a>
-              <a className="z-50" href="">
-                About
-              </a>
-            </div>
+      <div
+        className={`h-[100vh] w-[100%] bg-black flex justify-center items-center transition-all duration-3000 ease-in-out ${paddingClass}`}
+      >
+        <div className="h-[100%] relative flex justify-center items-center bg-[#ebebeb] rounded-2xl">
+          <div className="absolute flex top-[30%] ml-24 text-8xl text-gray-300 z-0 tracking-[60px] text-shadow-2xs font-bold orbitron-text">
+            FUTURISTIC
           </div>
-          <Button className="h-14 w-26 text-xl cursor-pointer z-50">
-            Signup
-          </Button>
+          <Navbar />
+          <Robot_model />
         </div>
-        <Robot_model />
       </div>
     </>
   );
---
+++ 3D-frontend/src/components/Navbar.tsx
@@ -0,0 +1,55 @@
+import { Search } from "lucide-react";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="absolute top-0 h-[60px] w-[100%] text-5xl flex justify-between px-3 items-center">
+        <img
+          src="A.png"
+          alt="Logo"
+          className="h-20 mt-12 animate-slide-from-top z-10"
+        />
+        <div className="text-sm gap-12 ml-90 flex font-bold animate-slide-from-top text-white z-50">
+          <Button className="hover:text-gray-800 hover:bg-[#ebebeb] z-50 cursor-pointer bg-black border-2 text-[#ebebeb] rounded-full">
+            Home
+          </Button>
+
+          <Button className="hover:text-gray-800 hover:bg-[#ebebeb] z-50 cursor-pointer bg-black border-2 text-[#ebebeb] rounded-full">
+            Contact
+          </Button>
+          <Button className="hover:text-gray-800 hover:bg-[#ebebeb] z-50 cursor-pointer bg-black border-2 text-[#ebebeb] rounded-full">
+            FAQs
+          </Button>
+        </div>
+        <div className="flex justify-between items-center w-[400px]">
+          <div className="z-50 flex justify-center items-center gap-2">
+            <Input className="border-gray-800 border-2 rounded-full bg-[#ebebeb] text-gray-800 font-bold" placeholder="Search..."/>
+            <Search size={30} color="black" className="cursor-pointer"/>
+          </div>
+          <Button className="h-10 font-bold rounded-4xl cursor-pointer z-50 animate-slide-from-top bg-[#ebebeb] border-2 text-gray-800 hover:text-[#ebebeb]">
+            Signup
+          </Button>
+        </div>
+        <img
+          src="/topShape.png"
+          alt="top shape"
+          className="absolute h-[200px] w-[900px] -top-[0.1px] right-[212px] object-cover"
+        />
+        <img
+          src="/topShape-2.png"
+          alt="top shape"
+          className="absolute h-[115px] -top-[2px] right-[0px] object-contain"
+        />
+        <img
+          src="/topShape-2.png"
+          alt="top shape"
+          className="absolute h-[110px] -top-[2px] -left-[1px] transform scale-x-[-1] object-contain"
+        />
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
